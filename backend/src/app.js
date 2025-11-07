fd188ec34099636acb21f933b0523013d139f1e2
Yash Detani <yuvrajmages@gmail.com>
2025-11-08 02:47:24 +0530

    search videos on pexels

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
-})
+// app.get("/", (req,res) => {
+//     res.send("Hello World");
+// })
 
 app.use('/api/auth', authRoutes); 
 app.use('/api/food', foodRoutes);
