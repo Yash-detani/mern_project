be34c6f1a29a7f607b5d7778d158cbb258405117
Yash Detani <yuvrajmages@gmail.com>
2025-11-08 19:46:56 +0530

    ImageKit

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,7 +1,14 @@
 const foodModel = require('../models/food.model');
 
 async function createFood(req,res){
+    console.log(req.foodPartner);
 
+    console.log(req.body)
+    console.log(req.file);
+
+    res.send("food item created");
 }
 
-module.exports = createFood;
+module.exports = {
+    createFood
+};
